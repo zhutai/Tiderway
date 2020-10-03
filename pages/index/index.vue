38cb03bbf148fe2f35f28a6d9a452fd93b200f1e
zhutai <zhutai@outlook.com>
2020-10-03 23:09:15 +0800

    feat: 项目部分文件上传

---
+++ pages/index/index.vue
@@ -0,0 +1,399 @@
+<template>
+	<view class="container">
+		
+		<!--  自定义顶部栏 -->
+		<uni-nav-bar fixed :border="false">
+			<view class="user-center">
+				<image class="user-avatar" src="../../static/missing-face.png"></image>
+			</view>
+			<view class="bar-text" slot="left">切换设备</view>
+			<view class="bar-text" slot="right">添加设备</view>
+		</uni-nav-bar>
+		
+		<!--  设备状态显示 -->
+		<view class="device-info">
+			<view class="device-icon">
+				<uni-icons type="info" color="#8f8f94" size="48" />
+			</view>
+			<view class="device-text">
+				<text>很遗憾您的设备不在线~~</text>
+			</view>
+			<view class="device-btn">
+				<button class="mini-btn" type="default" :plain="true" size="mini">查看原因</button>
+			</view>
+		</view>
+		
+		<view class="example-body">
+			<uni-section title="健康数据" type="line">
+				<view class="cell-more">
+					<text class="cell-text">更多</text>
+					<text class="cell-icon yticon icon-you"></text>
+				</view>
+			</uni-section>
+			<view class="grid-list">
+				<uni-grid :column="3" :show-border="false" :square="false" @change="change">
+					<uni-grid-item v-for="(item ,index) in healthyList" :key="index">
+						<view class="grid-item-box">
+							<text class="block iconfont" :style="{ color: item.color, fontSize: '24px' }" :class="item.icon"></text>
+							<text class="block text">{{item.num}}</text>
+							<text class="block text">{{item.name}}</text>
+						</view>
+					</uni-grid-item>
+				</uni-grid>
+			</view>
+		</view>
+		
+		<view class="example-body">
+			<uni-section title="辐射数据" type="line">
+				<view class="cell-more">
+					<text class="cell-text">更多</text>
+					<text class="cell-icon yticon icon-you"></text>
+				</view>
+			</uni-section>
+			<view class="grid-list">
+				<uni-grid :column="3" :show-border="false" :square="false" @change="change">
+					<uni-grid-item v-for="(item ,index) in radiaList" :key="index">
+						<view class="grid-item-box">
+							<text class="block iconfont" :style="{ color: item.color, fontSize: '24px' }" :class="item.icon"></text>
+							<text class="block text">{{item.num}}</text>
+							<text class="block text">{{item.name}}</text>
+							<text class="block text">({{item.unit}})</text>
+						</view>
+					</uni-grid-item>
+				</uni-grid>
+			</view>
+		</view>
+		
+		<!--  设备功能表模块 -->
+		<view class="example-body">
+			<uni-section title="设备功能表" type="line"></uni-section>
+			<view class="grid-list">
+				<uni-grid :column="3" :show-border="false" :square="false" @change="change">
+					<uni-grid-item class="cate-section" v-for="(item ,index) in optionList" :key="index">
+						<view class="cate-item">
+							<text class="block iconfont" :style="{ color: '#4399fc', fontSize: '30px' }" :class="item.icon"></text>
+							<!-- <image :src="item.imageUrl"></image> -->
+							<text style="padding-top: 12rpx;">{{ item.name }}</text>
+						</view>
+					</uni-grid-item>
+				</uni-grid>
+			</view>
+		</view>
+		
+		<view class="ad-1">
+			<image src="/static/temp/ad1.jpg" mode="scaleToFill"></image>
+		</view>
+
+	</view>
+</template>
+
+<script>
+import uniNavBar from "@/components/uni-nav-bar/uni-nav-bar.vue"
+
+const healthyList = [
+	{
+		name: '步',
+		unit: 'step',
+		icon: 'iconbushu',
+		key: 'step',
+		color: '#4399fc',
+		num: 3264
+	},{
+		name: '心率',
+		unit: 'bpm',
+		icon: 'iconjiankang',
+		key: 'bpm',
+		color: '#95F204',
+		num: 90
+	},{
+		name: '血压',
+		unit: 'mmHg',
+		icon: 'iconxieya',
+		color: '#F59A23',
+		key: 'mmHg',
+		num: 36.5
+	},
+]
+
+const radiaList = [
+	{
+		name: '峰值剂量',
+		unit: 'µSv/h',
+		icon: 'iconfengzhiguzhituli',
+		key: 'step',
+		color: '#4399fc',
+		num: 3264
+	},{
+		name: '累计时间',
+		unit: 'Seconds',
+		icon: 'iconshijian1',
+		key: 'bpm',
+		color: '#95F204',
+		num: 90
+	},{
+		name: '累计剂量',
+		unit: 'µSv/h',
+		icon: 'iconleiji',
+		color: '#F59A23',
+		key: 'mmHg',
+		num: 36.5
+	},
+]
+
+const optionList = [
+	{
+		icon: 'iconiconfontdingwei3',
+		name: '地图定位',
+	},{
+		icon: 'iconhuodongguiji',
+		name: '活动轨迹',
+	},{
+		icon: 'iconboda',
+		name: '拨打设备',
+	},{
+		icon: 'iconqinqinghaomachaxun',
+		name: ' 亲情号码',
+	},{
+		icon: 'iconshebeishezhi',
+		name: ' 设备设置'
+	}
+]
+
+export default {
+
+	data() {
+		return {
+			titleNViewBackground: '',
+			swiperCurrent: 0,
+			swiperLength: 0,
+			carouselList: [],
+			goodsList: [],
+			healthyList,
+			optionList,
+			radiaList
+		};
+	},
+	components: {
+		uniNavBar
+	},
+	onLoad() {
+		this.loadData();
+	},
+	methods: {
+		/**
+		 * 请求静态数据只是为了代码不那么乱
+		 * 分次请求未作整合
+		 */
+		async loadData() {
+			let carouselList = await this.$api.json('carouselList');
+			this.titleNViewBackground = carouselList[0].background;
+			this.swiperLength = carouselList.length;
+			this.carouselList = carouselList;
+			
+			let goodsList = await this.$api.json('goodsList');
+			this.goodsList = goodsList || [];
+		},
+	},
+	// #ifndef MP
+	// 标题栏input搜索框点击
+	onNavigationBarSearchInputClicked: async function(e) {
+		this.$api.msg('点击了搜索框');
+	},
+	//点击导航栏 buttons 时触发
+	onNavigationBarButtonTap(e) {
+		const index = e.index;
+		if (index === 0) {
+			this.$api.msg('点击了扫描');
+		} else if (index === 1) {
+			// #ifdef APP-PLUS
+			const pages = getCurrentPages();
+			const page = pages[pages.length - 1];
+			const currentWebview = page.$getAppWebview();
+			currentWebview.hideTitleNViewButtonRedDot({
+				index
+			});
+			// #endif
+			uni.navigateTo({
+				url: '/pages/notice/notice'
+			})
+		}
+	}
+	// #endif
+}
+</script>
+
+<style lang="scss">
+	
+	page {
+		background: #f5f5f5;
+	}
+	.m-t{
+		margin-top: 16upx;
+	}
+	
+	.container {
+		background: #f5f5f5;
+	}
+	
+	.block {
+		display: block;
+		text-align: center;
+	}
+	
+	.text {
+		font-size: 12px;
+		margin-top: 4px;
+		color: #999;
+	}
+	
+	.bar-text {
+		width: auto;
+		font-size: 14px;
+		color: $font-color-spec;
+	}
+	
+	.example-body {
+		margin-bottom: 24rpx;
+		.grid-list {
+			// height: 1500upx;
+			/* #ifndef APP-NVUE */
+			display: flex;
+			/* #endif */
+			flex-direction: row;
+			flex-wrap: wrap;
+			justify-content: center;
+			padding-bottom: 24rpx;
+			font-size: 14px;
+			background-color: #ffffff;
+		}
+	}
+	
+	/deep/ .uni-navbar__header-container {
+		flex: none;
+		margin: 0 auto;
+		.user-center {
+			display: inline-flex;
+			image {
+				width: 42px;
+				height: 42px;
+				border-radius: 50%;
+			}
+		}
+	}
+	
+	.device-info {
+		padding: 16px;
+		text-align: center;
+		.device-icon {
+			font-size: 0;
+			padding-top: 12px;
+		}
+		.device-text {
+			font-size: 12px;
+			color: #C0C4CC;
+		}
+		.device-btn {
+			font-size: 0;
+			padding-top: 12px;
+			/deep/ uni-button[type=default] {
+				color: #C0C4CC;
+				transform: scale(0.8);
+				border: 1px solid #C0C4CC;
+			}
+		}
+	}
+	
+	.cell-more {
+		.cell-text, .cell-icon {
+			color: #C0C4CC;
+			align-self: center;
+			font-size: 12px;
+		}
+	}
+	
+	/* 分类 */
+	.cate-section {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		flex-wrap:wrap;
+		padding: 30upx 22upx; 
+		background: #fff;
+		.cate-item {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			font-size: $font-sm + 2upx;
+			color: $font-color-dark;
+		}
+		/* 原图标颜色太深,不想改图了,所以加了透明度 */
+		image {
+			width: 88upx;
+			height: 88upx;
+			margin-bottom: 14upx;
+			border-radius: 50%;
+			opacity: .7;
+			box-shadow: 4upx 4upx 20upx rgba(250, 67, 106, 0.3);
+		}
+	}
+	.ad-1{
+		padding: 4upx 30upx 24upx;
+		background: #fff;
+		.s-header{
+			display:flex;
+			align-items:center;
+			height: 92upx;
+			line-height: 1;
+			.s-img{
+				width: 140upx;
+				height: 30upx;
+			}
+			.tip{
+				font-size: $font-base;
+				color: $font-color-light;
+				margin: 0 20upx 0 40upx;
+			}
+			.timer{
+				display:inline-block;
+				width: 40upx;
+				height: 36upx;
+				text-align:center;
+				line-height: 36upx;
+				margin-right: 14upx;
+				font-size: $font-sm+2upx;
+				color: #fff;
+				border-radius: 2px;
+				background: rgba(0,0,0,.8);
+			}
+			.icon-you{
+				font-size: $font-lg;
+				color: $font-color-light;
+				flex: 1;
+				text-align: right;
+			}
+		}
+		.floor-list{
+			white-space: nowrap;
+		}
+		.scoll-wrapper{
+			display:flex;
+			align-items: flex-start;
+		}
+		.floor-item{
+			width: 150upx;
+			margin-right: 20upx;
+			font-size: $font-sm+2upx;
+			color: $font-color-dark;
+			line-height: 1.8;
+			image{
+				width: 150upx;
+				height: 150upx;
+				border-radius: 6upx;
+			}
+			.price{
+				color: $uni-color-primary;
+			}
+		}
+	}
+
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,181 @@
+<template>  
+   <view class="container">  
+		
+		<view class="cover-container">
+			<uni-list>
+				<uni-list-item title="畅想童年" note="60300074" showArrow thumb="../../static/missing-face.png" thumb-size="lg" />
+			</uni-list>
+			<!-- 浏览历史 -->
+			<view class="history-section icon">
+				<list-cell icon="iconfont iconxiaoxi" iconColor="#e07472" title="我的消息" tips=""></list-cell>
+				<list-cell icon="iconfont icontubiaozhizuomoban-1-05" iconColor="#5fcda2" title="我的设备" @eventClick="navTo('/pages/address/address')"></list-cell>
+				<list-cell icon="iconfont iconGroup" iconColor="#9789f7" title="我的数据" tips=""></list-cell>
+			</view>
+			<view class="history-section icon">
+				<list-cell icon="iconfont iconyijian" iconColor="#ee883b" title="意见反馈" tips=""></list-cell>
+				<list-cell icon="iconfont iconshebeishezhi" iconColor="#54b4ef" title="设置"></list-cell>
+				<list-cell icon="iconfont iconguanyu" iconColor="#e07472" title="关于" border="" @eventClick="navTo('/pages/set/set')"></list-cell>
+			</view>
+		</view>
+    </view>
+</template>  
+<script>  
+	import listCell from '@/components/mix-list-cell';
+  import { mapState } from 'vuex';  
+	let startY = 0, moveY = 0, pageAtTop = true;
+    export default {
+		components: {
+			listCell
+		},
+		data(){
+			return {
+				coverTransform: 'translateY(0px)',
+				coverTransition: '0s',
+				moving: false,
+			}
+		},
+		onLoad(){
+		},
+		// #ifndef MP
+		onNavigationBarButtonTap(e) {
+			const index = e.index;
+			if (index === 0) {
+				this.navTo('/pages/set/set');
+			}else if(index === 1){
+				// #ifdef APP-PLUS
+				const pages = getCurrentPages();
+				const page = pages[pages.length - 1];
+				const currentWebview = page.$getAppWebview();
+				currentWebview.hideTitleNViewButtonRedDot({
+					index
+				});
+				// #endif
+				uni.navigateTo({
+					url: '/pages/notice/notice'
+				})
+			}
+		},
+		// #endif
+        computed: {
+			...mapState(['hasLogin','userInfo'])
+		},
+        methods: {
+
+			/**
+			 * 统一跳转接口,拦截未登录路由
+			 * navigator标签现在默认没有转场动画，所以用view
+			 */
+			navTo(url){
+				if(!this.hasLogin){
+					url = '/pages/public/login';
+				}
+				uni.navigateTo({  
+					url
+				})  
+			}, 
+	
+			/**
+			 *  会员卡下拉和回弹
+			 *  1.关闭bounce避免ios端下拉冲突
+			 *  2.由于touchmove事件的缺陷（以前做小程序就遇到，比如20跳到40，h5反而好很多），下拉的时候会有掉帧的感觉
+			 *    transition设置0.1秒延迟，让css来过渡这段空窗期
+			 *  3.回弹效果可修改曲线值来调整效果，推荐一个好用的bezier生成工具 http://cubic-bezier.com/
+			 */
+			coverTouchstart(e){
+				if(pageAtTop === false){
+					return;
+				}
+				this.coverTransition = 'transform .1s linear';
+				startY = e.touches[0].clientY;
+			},
+			coverTouchmove(e){
+				moveY = e.touches[0].clientY;
+				let moveDistance = moveY - startY;
+				if(moveDistance < 0){
+					this.moving = false;
+					return;
+				}
+				this.moving = true;
+				if(moveDistance >= 80 && moveDistance < 100){
+					moveDistance = 80;
+				}
+		
+				if(moveDistance > 0 && moveDistance <= 80){
+					this.coverTransform = `translateY(${moveDistance}px)`;
+				}
+			},
+			coverTouchend(){
+				if(this.moving === false){
+					return;
+				}
+				this.moving = false;
+				this.coverTransition = 'transform 0.3s cubic-bezier(.21,1.93,.53,.64)';
+				this.coverTransform = 'translateY(0px)';
+			}
+        }  
+    }  
+</script>  
+<style lang='scss'>
+	%flex-center {
+	 display:flex;
+	 flex-direction: column;
+	 justify-content: center;
+	 align-items: center;
+	}
+	%section {
+	  display:flex;
+	  justify-content: space-around;
+	  align-content: center;
+	  background: #fff;
+	  border-radius: 10upx;
+	}
+
+	.cover-container{
+		background: $page-color-base;
+		/* margin-top: -150upx; */
+		/* padding: 0 30upx; */
+		position:relative;
+		background: #f5f5f5;
+		/* padding-bottom: 20upx; */
+		.arc{
+			position:absolute;
+			left: 0;
+			top: -34upx;
+			width: 100%;
+			height: 36upx;
+		}
+	}
+	.history-section{
+		/* padding: 30upx 0 0; */
+		margin-top: 24upx;
+		/* margin-top: 20upx; */
+		background: #fff;
+		/* border-radius:10upx; */
+		.sec-header{
+			display:flex;
+			align-items: center;
+			font-size: $font-base;
+			color: $font-color-dark;
+			line-height: 40upx;
+			margin-left: 30upx;
+			.yticon{
+				font-size: 44upx;
+				color: #5eba8f;
+				margin-right: 16upx;
+				line-height: 40upx;
+			}
+		}
+		.h-list{
+			white-space: nowrap;
+			padding: 30upx 30upx 0;
+			image{
+				display:inline-block;
+				width: 160upx;
+				height: 160upx;
+				margin-right: 20upx;
+				border-radius: 10upx;
+			}
+		}
+	}
+	
+</style>
